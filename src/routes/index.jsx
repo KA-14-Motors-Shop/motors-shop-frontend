d207785bccac2ef777d452e4bbed3c22ed665a09
Guilherme <guilherme.llopreti@gmail.com>
2022-08-09 03:11:58 -0300

    feat: mobile product page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,13 @@ import { Switch } from "react-router-dom";
 import Route from "./route";
 import ProductCard from "../components/Cards/ProductCard";
 import Home from "../pages/Home";
+import ProductPage from "../pages/Product";
 
 const Routes = () => {
   return (
     <Switch>
       <Route isPublic component={Home} exact path="/" />
+      <Route isPublic component={ProductPage} exact path="/product" />
       <Route isPublic component={ProductCard} exact path="/login" />
     </Switch>
   );
