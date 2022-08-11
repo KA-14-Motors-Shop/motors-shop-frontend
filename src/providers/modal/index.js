6294b1da976a35788e717fd44507fb16205ec5a0
Guilherme <guilherme.llopreti@gmail.com>
2022-08-11 11:19:25 -0300

    fix: modal provider initial value

--- src/providers/modal/index.js
+++ src/providers/modal/index.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const ModalContext = createContext();
 
 export const ModalProvider = ({ children }) => {
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <ModalContext.Provider value={{ showModal, setShowModal }}>
