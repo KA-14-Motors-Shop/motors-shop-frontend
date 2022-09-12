58786c8c2b2b64563b56c067ee9a061503700c06
GabrielRD <grd342@outlook.com>
2022-09-12 01:19:43 -0300

    tests: register-component/adding tests

--- src/__tests__/components/registerCard.spec.js
+++ src/__tests__/components/registerCard.spec.js
@@ -9,13 +9,19 @@ test("Should be able to render the CardRegister", () => {
     expect(screen.getByTestId("Register-Container")).toBeTruthy()
 })
 
-test("Should be able to render the h2 title", () => {
+test("Should be able to fixed texts in component", () => {
     render(<CardRegister/>)
 
     const cadastro = screen.getByText("Cadastro")
     const heading = screen.getByText(/Informações pessoais/i)
+    const informacoes = screen.getByText("Informações de endereço")
+    const tipo = screen.getByText("Tipo de conta")
+
+
     expect(heading).toBeTruthy()
     expect(cadastro).toBeTruthy()
+    expect(informacoes).toBeTruthy()
+    expect(tipo).toBeTruthy()
 
 })
 
