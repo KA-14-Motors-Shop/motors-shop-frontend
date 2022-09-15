7abe665fd833a039f9c380e722cb64ee76f14d1e
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-09-15 19:11:31 -0300

    fix: edit pf modal state no longer starts at true

--- src/providers/editPfModal/index.js
+++ src/providers/editPfModal/index.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const EditPfModalContext = createContext();
 
 export const EditPfModalProvider = ({ children }) => {
-  const [editPfModal, setEditPfModal] = useState(true);
+  const [editPfModal, setEditPfModal] = useState(false);
 
   return (
     <EditPfModalContext.Provider value={{ editPfModal, setEditPfModal }}>
