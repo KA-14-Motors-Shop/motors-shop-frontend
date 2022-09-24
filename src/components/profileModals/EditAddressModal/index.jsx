4b651bd1ff3c0e056ee9f34af9f80a0d3fe10cf0
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-09-24 17:21:12 -0300

    feat: further edit adress modal setup

--- src/components/profileModals/EditAddressModal/index.jsx
+++ src/components/profileModals/EditAddressModal/index.jsx
@@ -1,14 +1,45 @@
 import AnuncioModal from "../../modal";
 import { EditAdrsContainer } from "./styles";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 
 const EditAddressModal = ({ modalState, setModalState }) => {
+  const schema = yup.object().shape({
+    cep: yup.string(),
+    state: yup.string(),
+    city: yup.string(),
+    street: yup.string(),
+    number: yup.number(),
+    complement: yup.string(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmitFunction = (data) => {
+    console.log(data);
+  };
+
   return (
     <EditAdrsContainer>
       <AnuncioModal
         title="Editar endereço"
         modalState={modalState}
         setModalState={setModalState}
-      ></AnuncioModal>
+      >
+        <form
+          className="edit__address__form"
+          onSubmit={handleSubmit(onSubmitFunction)}
+        >
+          <h6>Informações de endereço</h6>
+        </form>
+      </AnuncioModal>
     </EditAdrsContainer>
   );
 };
--- src/components/profileModals/EditAddressModal/styles.js
+++ src/components/profileModals/EditAddressModal/styles.js
@@ -4,4 +4,16 @@ export const EditAdrsContainer = styled.div`
   svg {
     cursor: pointer;
   }
+
+  h6 {
+    width: 100%;
+    font-size: 14px;
+    font-weight: 500;
+  }
+
+  form {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 35px;
+  }
 `;
